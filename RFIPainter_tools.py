33a5f3e9bb0e2badafb178abe358f2493137206c
Josh Kerrigan <joshua_kerrigan@brown.edu>
2018-10-04 19:07:09 -0400

    Initial commit adding painter tools and run script.

---
+++ RFIPainter_tools.py
@@ -0,0 +1,317 @@
+import matplotlib
+matplotlib.use('TKAgg')
+import pylab as plt
+from scipy import signal
+import aipy as a
+import numpy as np
+from glob import glob
+import random
+import pyuvdata
+from matplotlib.patches import Rectangle
+import h5py
+
+Plot=False
+pull_samples=False
+def loadAipyData(time):
+    HERAlist = glob('./zen.*.*.yy.HH.uvOCR')
+    HERAdata = []
+    times = []
+    for l in ['11_121']: #,'10_31','31_105','10_105','9_31','9_105','10_31']:
+        data = []
+        for k in HERAlist:
+            uvHERA = a.miriad.UV(k)
+            a.scripting.uv_selector(uvHERA, l, 'xx')
+            for p,d,f in uvHERA.all(raw=True):
+                data.append(d)
+                times.append(uvHERA['lst'])
+        if l == '11_121':
+            HERAdata = [data]
+        else:
+            HERAdata.append(data)
+    print n.shape(HERAdata)
+    HERAdata = n.array(HERAdata)
+    times = n.array(times)
+    return HERAdata,times
+
+def loadH5():
+    f = h5py.File('/Users/josh/Desktop/Organize/UPennML/ml_rfi/RealVisRFI_v3.h5')
+    return f['data'][:],np.logical_not(f['flag'])
+
+def loadPYUVdata():
+    files = np.array(glob('./zen.*.uvSLIM'))
+    info = {}
+    ct = 0
+    rnd_3 = np.random.randint(len(files),size=5)
+    file_cut = files[rnd_3]
+    # grab 10 files at random
+#    f_rnd = np.random.randint(0,len(files),size=6)
+    for f in file_cut:#f_rnd]:
+        print(f)
+        #info[f] = {'antpairs' : {}}
+        uv = pyuvdata.UVData()
+        uv.read_miriad(f,run_check_acceptability=False,run_check=False,check_extra=False)
+        antpairs = uv.get_antpairs()
+        for i in range(3):
+        #    print(ct)
+            rnd = np.random.randint(len(antpairs))
+            ap1,ap2 = antpairs[rnd]
+            pos1 = uv.antenna_positions[uv.antenna_numbers==ap1]
+            pos2 = uv.antenna_positions[uv.antenna_numbers==ap2]
+            bl_len = np.round(np.sqrt(np.sum((pos1-pos2)**2)),2)
+            info[f+'_{0}'.format(ct)] = '{0}_blen_{1}'.format(ct,bl_len)#str(ct)+'_blen_'+str() #['antpairs'][ct] = antpairs[rnd] #antpairs[rnd] : ct}
+            if f == file_cut[0] and i == 0:#f_rnd[0]]:# and i == 0:
+                HERAdata = [uv.get_data(antpairs[rnd]).squeeze()]
+            else:
+                HERAdata.append(uv.get_data(antpairs[rnd]).squeeze())
+            ct+=1
+        del(uv)
+    print('Dataset size: ',np.shape(HERAdata))
+    return HERAdata,info
+
+plt.ion()
+options={'y':-1,
+    'n':100}
+
+class Annotate(object):
+    def __init__(self):
+        self.ax = plt.gca()
+#        self.ax = 
+        self.rect = Rectangle((0,0), 1, 1)
+        self.x0 = None
+        self.y0 = None
+        self.x1 = None
+        self.y1 = None
+        self.ax.add_patch(self.rect)
+        self.ax.figure.canvas.mpl_connect('button_press_event', self.on_press)
+        self.ax.figure.canvas.mpl_connect('button_release_event', self.on_release)
+        self.ax.figure.canvas.mpl_connect('key_press_event', self.quit)
+
+    def on_press(self, event):
+        #print 'press'
+        self.x0 = event.xdata
+        self.y0 = event.ydata
+
+    def on_release(self, event):
+        #print 'release'
+        self.x1 = event.xdata
+        self.y1 = event.ydata
+        self.rect.set_width(self.x1 - self.x0)
+        self.rect.set_height(self.y1 - self.y0)
+        self.rect.set_xy((self.x0, self.y0))
+        self.ax.figure.canvas.draw()
+
+class Painter:
+    def __init__(self,data,info,mask=None,delay_only=False):
+        self.ct = 0
+        if delay_only:
+            self.fig2 = plt.figure()
+            self.canvas2 = self.fig2.canvas
+        else:
+            self.fig = plt.figure()
+            self.fig2 = plt.figure()
+            self.fig3 = plt.figure()
+            self.canvas = self.fig.canvas
+            self.canvas2 = self.fig2.canvas
+            self.canvas3 = self.fig3.canvas
+        self.data = np.copy(data)
+        self.keep = True
+        self.delay_only = delay_only
+        if np.any(mask!=None):
+            self.mask = mask
+        else:
+            self.mask = np.ones_like(self.data).astype(int)
+        self.info = info
+        self.curr_mask = self.mask[0]
+        self.undomask = np.copy(self.curr_mask)
+        self.curr_data = self.data[0]
+        if delay_only:
+            self.ax2 = self.fig2.gca()
+        else:
+            self.ax = self.fig.gca()
+            self.ax2 = self.fig2.gca()
+            self.ax3 = self.fig3.gca()
+        if delay_only:
+            self.cid7_2 = self.canvas2.mpl_connect('key_press_event', self.pop)
+        else:
+            self.cid1 = self.canvas.mpl_connect('button_press_event', self.on_press)
+            self.cid2 = self.canvas.mpl_connect('button_release_event', self.on_release)
+            self.cid3 = self.canvas.mpl_connect('button_press_event', self.undo)
+            self.cid4 = self.canvas.mpl_connect('key_press_event', self.on_key_press)
+            self.cid5 = self.canvas.mpl_connect('key_release_event', self.on_key_release)
+            self.cid6 = self.canvas.mpl_connect('key_press_event', self.next)
+            self.cid7 = self.canvas.mpl_connect('key_press_event', self.pop)
+        
+            self.cid1_ = self.canvas3.mpl_connect('button_press_event', self.on_press)
+            self.cid2_ = self.canvas3.mpl_connect('button_release_event', self.on_release)
+            self.cid3_ = self.canvas3.mpl_connect('button_press_event', self.undo)
+            self.cid4_ = self.canvas3.mpl_connect('key_press_event', self.on_key_press)
+            self.cid5_ = self.canvas3.mpl_connect('key_release_event', self.on_key_release)
+            self.cid6_ = self.canvas3.mpl_connect('key_press_event', self.next)
+            self.cid7_ = self.canvas3.mpl_connect('key_press_event', self.pop)
+
+        logdata = np.log10(np.abs(self.curr_data))*np.abs(self.curr_mask)
+
+        maxD = np.max(np.abs(logdata))
+        minD = np.min(np.abs(logdata))
+        if self.delay_only:
+            self.delay_plot = self.ax2.imshow(np.log10(np.abs(self.delayTrans())),aspect='auto')
+            self.ax2.set_title('Delay Spectrum')
+            self.ax2.set_xlabel('Delay Bin')
+            self.ax2.set_ylabel('Time')
+        else:
+            self.amp_plot = self.ax.imshow(np.log10(np.abs(self.curr_data))*np.abs(self.curr_mask),aspect='auto')
+            self.ax.set_title('Visibility')
+            self.ax.set_xlabel('Freq.')
+            self.ax.set_ylabel('Time')
+            self.delay_plot = self.ax2.imshow(np.log10(np.abs(self.delayTrans())),aspect='auto')
+            self.ax2.set_title('Delay Spectrum')
+            self.ax2.set_xlabel('Delay Bin')
+            self.ax2.set_ylabel('Time')
+            self.phs_plot = self.ax3.imshow(np.angle(self.curr_data*self.curr_mask),aspect='auto')
+            self.ax3.set_title('Phase')
+            self.ax3.set_xlabel('Freq')
+            self.ax3.set_ylabel('Time')
+        self.x0 = None
+        self.y0 = None
+        self.x1 = None
+        self.y1 = None
+
+    def on_key_press(self, event):
+        if event.key == 'shift':
+            self.shift_is_held = True
+
+    def test(self, event):
+        if event.key == 'right':
+            print('Right Arrow')
+        if event.key == 'n':
+            print('n')
+
+    def pop(self, event):
+        if event.key == 'p':
+            if self.delay_only:
+                self.keep = False
+            else:
+                if self.info.has_key(self.ct):
+                    bad_key = [key_ for key_ in self.info.keys() if int(info[key_].split('_')[0]) == self.ct]
+                    self.info[bad_key[0]] = '{0}_blen_{1}'.format(-1,000)
+                else:
+                    self.info['000_'+str(self.ct)] = '{0}_blen_{1}'.format(-1,000)
+                self.ct += 1
+                self.curr_data = self.data[self.ct]
+                self.curr_mask = self.mask[self.ct]
+                if self.delay_only:
+                    self.delay_plot.set_data(np.log10(np.abs(self.delayTrans())))
+                    self.canvas2.draw()
+                else:
+                    self.amp_plot.set_data(np.log10(np.abs(self.curr_data))*np.abs(self.curr_mask))
+                    self.canvas.draw()
+                    self.delay_plot.set_data(np.log10(np.abs(self.delayTrans())))
+                    self.canvas2.draw()
+                    self.phs_plot.set_data(np.angle(self.curr_data*self.curr_mask))
+                    self.canvas3.draw()
+    
+    def next(self, event):
+        if event.key == 'right':
+            self.data[self.ct] = np.copy(self.curr_data)
+            self.mask[self.ct] = np.copy(self.curr_mask)
+            if not self.info.has_key(self.ct):
+                self.info['000_'+str(self.ct)] = '{0}_blen_{1}'.format(self.ct,000)
+            self.ct += 1
+            print(self.ct)
+            self.curr_data = self.data[self.ct]
+            self.curr_mask = self.mask[self.ct]
+            if self.delay_only:
+                self.delay_plot.set_data(np.log10(np.abs(self.delayTrans())))
+                self.canvas2.draw()
+            else:
+                self.amp_plot.set_data(np.log10(np.abs(self.curr_data))*np.abs(self.curr_mask))
+                self.canvas.draw()
+                self.delay_plot.set_data(np.log10(np.abs(self.delayTrans())))
+                self.canvas2.draw()
+                self.phs_plot.set_data(np.angle(self.curr_data*self.curr_mask))
+                self.canvas3.draw()
+
+    def on_key_release(self, event):
+        if event.key == 'shift':
+            self.shift_is_held = False
+
+    def onclick(self,event):
+        print('button=%d, x=%d, y=%d, xdata=%f, ydata=%f' % (event.button, event.x, event.y, event.xdata, event.ydata))
+        self.undomask = self.mask
+        self.curr_mask[int(event.ydata)+1,int(event.xdata)+1] = 0
+
+    def undo(self, event):
+        if event.button == 3:
+            self.curr_mask = self.undomask
+            logdata = np.log10(np.abs(self.curr_data))*np.abs(self.curr_mask)
+            minD = np.min(logdata)
+            maxD = np.max(logdata)
+            if self.delay_only:
+                self.delay_plot.set_data(np.log10(np.abs(self.delayTrans())))
+                self.canvas2.draw()
+            else:
+                self.amp_plot.set_data(np.log10(np.abs(self.curr_data))*np.abs(self.curr_mask))
+                self.canvas.draw()
+                self.delay_plot.set_data(np.log10(np.abs(self.delayTrans())))
+                self.canvas2.draw()
+                self.phs_plot.set_data(np.angle(self.curr_data*self.curr_mask))
+                self.canvas3.draw()
+
+    def on_press(self, event):
+        if event.button == 1:
+            self.undomask = np.copy(self.curr_mask)
+            self.x0 = event.xdata
+            self.y0 = event.ydata
+
+    def quit(self, event):
+        if event.button == 'q':
+            self.canvas.mpl_disconnect(self.cid1)
+            self.canvas.mpl_disconnect(self.cid2)
+            self.canvas.mpl_disconnect(self.cid3)
+            self.canvas.mpl_disconnect(self.cid4)
+            self.canvas.mpl_disconnect(self.cid5)
+            self.ax.close()
+            self.ax2.close()
+            
+
+    def on_release(self, event):
+        if plt.get_current_fig_manager().toolbar.mode != '': return
+
+        if event.button == 1:
+            self.x1 = event.xdata
+            self.y1 = event.ydata
+            if self.y0 >= self.y1 and self.x0 >= self.x1:
+                self.curr_mask[int(self.y1-1):int(self.y0),int(self.x1-1):int(self.x0)] = 0
+            elif self.y0 >= self.y1 and self.x1 >= self.x0:
+                self.curr_mask[int(self.y1-1):int(self.y0),int(self.x0):int(self.x1+1)] = 0
+            elif self.x0 >= self.x1 and self.y1 >= self.y0:
+                self.curr_mask[int(self.y0):int(self.y1+1),int(self.x0):int(self.x1+1)] = 0
+            else:
+                self.curr_mask[int(self.y0):int(self.y1+1),int(self.x0):int(self.x1+1)] = 0
+            logdata = np.log10(np.abs(self.curr_data))*np.abs(self.curr_mask)
+            minD = np.min(logdata)
+            maxD = np.max(logdata)
+            if self.delay_only:
+                self.delay_plot.set_data(np.log10(np.abs(self.delayTrans())))
+                self.canvas2.draw()
+            else:
+                self.amp_plot.set_data(np.log10(np.abs(self.curr_data))*np.abs(self.curr_mask))
+                self.canvas.draw()
+                self.delay_plot.set_data(np.log10(np.abs(self.delayTrans())))
+                self.canvas2.draw()
+        
+                self.phs_plot.set_data(np.angle(self.curr_data*self.curr_mask))
+                self.canvas3.draw()
+
+    
+    def get_data(self):
+        return self.data,self.mask,self.info
+
+    def return_keep(self):
+        return self.keep
+
+    def delayTrans(self):
+        bh = a.dsp.gen_window(1024,window='blackman-harris')
+        DATA = np.fft.fft(self.curr_data*np.abs(self.curr_mask)*bh,axis=1)
+        DATA_ = np.fft.fftshift(DATA,axes=1)
+        return DATA_
---
+++ run_RFIPainter.py
@@ -0,0 +1,149 @@
+import matplotlib
+matplotlib.use('TKAgg')
+import pylab as plt
+from scipy import signal
+import aipy as a
+import numpy as np
+from glob import glob
+import random
+import pyuvdata
+from matplotlib.patches import Rectangle
+import h5py
+import RFIPainter_tools as rpt
+
+"""
+RFI Painter allows you to import miriad or hdf5 files containing waterfall visibilities
+to flag RFI or genenerally any bad data.
+Important keys to note are:
+
+Left Click: place an RFI flag
+Left Click + Hold and Drag: place a rectanglar grouping of RFI flags
+Right Click: undo the most recently placed set of flags
+p: do not include waterfall into output dataset
+right arrow: go to next waterfall
+q: quit out of the canvasses, do this when you've reached the end of your dataset
+   and all of the waterfalls that were 'right arrowed' will be saved
+o: enter zoom mode
+
+"""
+
+
+
+Plot=False
+pull_samples=False
+file_locs = ''
+output_name = 'test.h5'#RealVisRFI_v4.h5'
+
+plt.ion()
+
+msk = None
+# Load data either from miriad files or hdf5 files
+
+#data,info = loadPYUVdata()
+data,mask = rpt.loadH5()
+
+info = {}
+print('Input data size: {} (# Samples, times, freqs)'.format(np.shape(data)))
+paint_session = rpt.Painter(data,info,mask=mask)
+plt.show(block=True)
+data,mask,updated_info = paint_session.get_data()
+
+data = [data[int(i.split('_')[-1])] for i in updated_info.keys() if int(updated_info[i].split('_')[0]) != -1]
+mask = [mask[int(i.split('_')[-1])] for i in updated_info.keys() if int(updated_info[i].split('_')[0]) != -1]
+print('Shape of data: {0} Shape of mask: {1}'.format(np.shape(data),np.shape(mask)))
+
+try:
+    updated_info_str = ['{}_'.format(i.split('/')[1].split('_')[0])+updated_info[i] for i in updated_info.keys() if int(updated_info[i].split('_')[0]) != -1]
+except:
+    updated_info_str = []
+print(updated_info_str)
+
+mask_ = np.copy(mask) 
+mask = np.logical_not(mask)
+
+hf = h5py.File(output_name,'a') # caution! this will attempt to overwrite data
+suffix='uv'
+if pull_samples:
+    for suffix in suffixes:
+        try:
+            dp = hf.create_group(suffix)
+        except:
+            dp = hf.require_group(suffix)
+        HERAdata = []
+        new_mask = []
+        obs_info = []
+        for key in updated_info.keys():
+            print key
+            obs = ('.').join(key.split('.')[:-1])+'.'+suffix+'SLIM'
+            uv = pyuvdata.UVData()
+            uv.read_miriad(obs)
+            antpairs = np.array(uv.get_antpairs())
+            order = updated_info[key]
+            rnd_aps = antpairs[random.sample(range(0,len(antpairs)),99)]
+            for ap in rnd_aps:
+                data_grab = uv.get_data(ap).squeeze()
+                refresher = Refresh(data_grab,info,mask=mask_[order],delay_only=True)
+                plt.show(block=True)
+                keep = refresher.return_keep()
+                if keep: 
+                    if len(HERAdata) == 0:
+                        HERAdata = [data_grab]
+                        new_mask = [mask[order]]
+                        obs_info = [obs+'_'+str(ap)]
+                    else:
+                        HERAdata.append(data_grab)
+                        new_mask.append(mask[order])
+                        obs_info.append(obs+'_'+str(ap))
+                else:
+                    print('Waterfall rejected.')
+                    continue
+        sh = np.shape(HERAdata)
+        try:
+            dp.create_dataset('data', data=HERAdata)
+            dp.create_dataset('flag', data=new_mask)
+            dp.create_dataset('meta', data=obs_info)
+        except:
+            old_data = np.copy(dp['data'][:])
+            old_flags = np.copy(dp['flag'][:])
+            old_info = np.copy(dp['meta'][:])
+            hf.close()
+            hf = h5py.File(output_name, 'w')
+            dp = hf.create_group(suffix)
+            new_data = np.vstack((old_data,HERAdata))
+            new_flags = np.vstack((old_flags,new_mask))
+            new_info = np.vstack((old_info,obs_info))
+            print(np.shape(new_data))
+            dp.create_dataset('data', data=new_data)
+            dp.create_dataset('flag', data=new_flags)
+            dp.create_dataset('meta', data=new_info)
+    hf.close()
+else:
+    try:
+        updated_info_str = ['{}_'.format(i.split('/')[1])+updated_info[i] for i in updated_info.keys()]
+        print(updated_info_str)
+    except:
+        pass
+    try:
+        dp = hf.create_group(suffix)
+    except:
+        dp = hf.require_group(suffix)
+    try:
+        dp.create_dataset('data', data=data)
+        dp.create_dataset('flag', data=mask)
+        dp.create_dataset('meta', data=updated_info_str)
+    except:
+        old_data = np.copy(dp['data'][:])
+        old_flags = np.copy(dp['flag'][:])
+        old_info = np.copy(dp['meta'][:])
+        hf.close()
+        hf = h5py.File(output_name, 'w')
+        dp = hf.create_group(suffix)
+        new_data = np.vstack((old_data,data))
+        new_flags = np.vstack((old_flags,mask))
+        new_info = np.append(old_info,updated_info_str)
+        print(np.shape(new_data))
+        dp.create_dataset('data', data=new_data)
+        dp.create_dataset('flag', data=new_flags)
+        dp.create_dataset('meta', data=new_info)
+    hf.close()
+
